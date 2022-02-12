174a491cb055341600ac79cc18f4255fd8211ac0
Alex-Rockwell <vin-ag@list.ru>
2022-02-12 20:57:23 +0400

    add pagination

--- src/Gallery.js
+++ src/Gallery.js
@@ -10,6 +10,8 @@ import UI_kit from "./UI_kit";
 
 function Gallery() {
   const [paintings, setPaintings] = useState([])
+  const [authors, setAuthors] = useState([])
+  const [locations, setLocations] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [currentPage, setCurrentPage] = useState(1)
   const [elementsPerPage, setElementsPerPage] = useState(9)
@@ -18,20 +20,46 @@ function Gallery() {
   useEffect(() => {
     async function getPaintings() {
       setIsLoading(true)
-      const res = await axios.get(`https://test-front.framework.team/paintings`)
-      setPaintings(res.data)
+      const res1 = await axios.get(`https://test-front.framework.team/paintings`)
+      const res2 = await axios.get(`https://test-front.framework.team/authors`)
+      const res3 = await axios.get(`https://test-front.framework.team/locations`)
+      setPaintings(res1.data)
+      setAuthors(res2.data)
+      setLocations(res3.data)
       setIsLoading(false)
     }
     getPaintings()
-    }, [])
-  console.log(paintings)
+  }, [])
+  
+  console.log('Use effect-1 paintings', paintings)
+
+  // Add authors and locations
+  useEffect(() => {
+    setPaintings((prev) => {
+      const newarr = prev.map(p => {
+        let val = authors.find(a => a.id === p.authorId)
+        return {...p, authorName: val.name}
+      })
+      return newarr
+    })
+  }, [authors])
+  useEffect(() => {
+    setPaintings((prev) => {
+      const newarr = prev.map(p => {
+        let val2 = locations.find(l => l.id === p.locationId)
+        return {...p, locationName: val2.location}
+      })
+      return newarr
+    })
+  }, [locations])
 
   // Create elements
   const paintingsEls = paintings.map(el => <Card 
     key={uuidv4()} 
     imageUrl={el.imageUrl} 
     cardName={el.name}
-    isLoading={isLoading}/>)
+    isLoading={isLoading}
+  />)
 
   const loader = <h1>Loading...</h1>
 
@@ -40,13 +68,24 @@ function Gallery() {
   const indexOfFirstElt = indexOfLastElt - elementsPerPage
   const currentElts = paintingsEls.slice(indexOfFirstElt, indexOfLastElt)
 
+  // Change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  }
+
+
+
   return (
     <div>
       <div className="gallery__card-container">
         {isLoading ? loader : currentElts}
       </div>
       <div>
-        <Pagination_c elementsPerPage={elementsPerPage} totalElements={paintings.length}/>
+        <Pagination_c 
+          elementsPerPage={elementsPerPage} 
+          totalElements={paintings.length}
+          paginate={paginate}
+        />
       </div>
       <p>{JSON.stringify(paintings)}</p>
       <UI_kit></UI_kit>
@@ -55,3 +94,4 @@ function Gallery() {
 }
 
 export default Gallery
+
--- src/Pagination_c.js
+++ src/Pagination_c.js
@@ -1,32 +1,64 @@
+import { useEffect, useState } from "react"
+import {ReactComponent as LeftArrow2} from './arrowLeft2.svg'
+import {ReactComponent as LeftArrow1} from './arrowLeft1.svg'
+import {ReactComponent as RightArrow1} from './arrowRight1.svg'
+import {ReactComponent as RightArrow2} from './arrowRight2.svg'
 
 
-function Pagination_c({elementsPerPage, totalElements}) {
+function Pagination_c({elementsPerPage, totalElements, paginate}) {
   const pageNumbers = []
-  console.log('elementsPerPage', elementsPerPage)
-  console.log('totalElements', totalElements)
+  const [currentPage, setCurrentPage] = useState(1)
 
   for (let i = 1; i < Math.ceil(totalElements / elementsPerPage); i++) {
     pageNumbers.push(i)
   }
-  console.log(pageNumbers)
+
+  const handlePageNumber = (number) => {
+    setCurrentPage(number)
+  }
+  
+  useEffect(() => {
+    paginate(currentPage)
+  }, [currentPage])
+
+  const isBeginActive = (currentPage === pageNumbers[0]) ? false : true
+  const isEndActive = (currentPage === pageNumbers[pageNumbers.length - 1]) ? false : true
+
+  const classBeginBtns = `pagination__item ${isBeginActive ? 'pagination__item--hoverable' : 'pagination__item--disable'}`
+  const classEndBtns = `pagination__item ${isEndActive ? 'pagination__item--hoverable' : 'pagination__item--disable'}`
+
   return (
-      <div>
-
-            <nav>
-              <ul className="pagination">
-              {pageNumbers.map(number => {
-                return (
-                <li key={number} className="pagination__item">
-                  <a className="pagination__link">
+    <div>
+      <nav>
+        <ul className="pagination">
+          <li onClick={() => setCurrentPage(1)} className={classBeginBtns + ' pagination__item--left'}>
+            <LeftArrow2 />
+          </li>
+          <li onClick={() => setCurrentPage(prev => prev - 1)} className={classBeginBtns}>
+            <LeftArrow1 />
+          </li>
+          {pageNumbers.map(number => {
+            return (
+              <>
+                <li 
+                  key={number} 
+                  className={`pagination__item ${(number === currentPage) ? 'pagination__item--active' : 'pagination__item--hoverable'}`}
+                  onClick={() => handlePageNumber(number)}
+                >
                     {number}
-                  </a>
                 </li>
-                )
-              })}
-              </ul>
-            </nav>
-
-      </div>
+              </>
+            )
+          })}
+          <li onClick={() => setCurrentPage(prev => prev + 1)} className={classEndBtns}>
+            <RightArrow1 />
+          </li>
+          <li onClick={() => setCurrentPage(pageNumbers.length)} className={classEndBtns + ' pagination__item--right'}>
+            <RightArrow2 />
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
